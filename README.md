0d67a1a3d51c97ab6e67484e23bb54655fc597ac
Kim Levin <kbussie1@montgomerycollege.edu>
2020-05-19 01:17:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### This SpringBoot application is an implementation of an assignment from Java Bootcamp. 
+
+It provides a form for the user to submit information about a car. Then it displays the values entered by the user on the page.
---
+++ src/main/java/com/example/demo/SpringBoot204Pt2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot204Pt2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot204Pt2Application.class, args);
+    }
+
+}
